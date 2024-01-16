03ad0f3a58f3d8a88c1fe5341e276abb991c7da8
Akaikenlol <keithken972@gmail.com>
2024-01-16 20:11:00 +0630

    feat: Form Submission and Actions

--- components/shared/DropDown.tsx
+++ components/shared/DropDown.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { startTransition, useState } from "react";
+import React, { startTransition, useEffect, useState } from "react";
 import {
 	Select,
 	SelectContent,
@@ -21,6 +21,10 @@ import {
 } from "@/components/ui/alert-dialog";
 import { ICategory } from "@/lib/database/models/category.model";
 import { Input } from "../ui/input";
+import {
+	createCategory,
+	getAllCategories,
+} from "@/lib/actions/category.actions";
 
 type DropDownProps = {
 	onChangeHandler?: () => void;
@@ -31,7 +35,23 @@ const DropDown = ({ onChangeHandler, value }: DropDownProps) => {
 	const [categories, setCategories] = useState<ICategory[]>([]);
 	const [newCategory, setNewCategory] = useState("");
 
-	const handleAddCategory = () => {};
+	const handleAddCategory = () => {
+		createCategory({
+			categoryName: newCategory.trim(),
+		}).then((category) => {
+			setCategories((prevState) => [...prevState, category]);
+		});
+	};
+
+	useEffect(() => {
+		const getCategories = async () => {
+			const categoryList = await getAllCategories();
+
+			categoryList && setCategories(categoryList as ICategory[]);
+		};
+
+		getCategories();
+	}, []);
 
 	return (
 		<Select onValueChange={onChangeHandler} defaultValue={value}>
@@ -51,7 +71,7 @@ const DropDown = ({ onChangeHandler, value }: DropDownProps) => {
 					))}
 				<AlertDialog>
 					<AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">
-						Open
+						Add New Category
 					</AlertDialogTrigger>
 					<AlertDialogContent className="bg-white">
 						<AlertDialogHeader>
