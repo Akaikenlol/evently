1e9662814258e4157b8b1b0eceae38342435af73
Akaikenlol <keithken972@gmail.com>
2024-01-18 19:00:41 +0630

    feat: Card

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -34,7 +34,11 @@ const Collection = ({
 
 							return (
 								<li key={event._id} className="flex justify-center">
-									<Card />
+									<Card
+										event={event}
+										hasOrderLink={hasOrderLink}
+										hidePrice={hidePrice}
+									/>
 								</li>
 							);
 						})}
