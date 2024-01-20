9b0307c70bb9ce3958658fa7c2abde0c1e2c07fc
Akaikenlol <keithken972@gmail.com>
2024-01-20 21:35:07 +0630

    feat: Implementing delay Debounce Functionality at Search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.actions";
 import Image from "next/image";
@@ -48,7 +49,8 @@ export default async function Home() {
 				</h2>
 
 				<div className="flex flex-col w-full gap-5 md:flex-row">
-					Search CategoryFilter
+					<Search />
+					CategoryFilter
 				</div>
 
 				<Collection
