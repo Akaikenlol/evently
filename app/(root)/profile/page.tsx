ddd2890e3d6f09b546a89f3eec058cbee8e88e61
Akaikenlol <keithken972@gmail.com>
2024-01-20 21:11:25 +0630

    feat: Checkout, strip implementation

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,15 +1,26 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.actions";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+import { IOrder } from "@/lib/database/models/order.model";
+import { SearchParamProps } from "@/types";
 import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 
-const ProfilePage = async () => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
 	const { sessionClaims } = auth();
 	const userId = sessionClaims?.userId as string;
 
-	const organizedEvents = await getEventsByUser({ userId, page: 1 });
+	const ordersPage = Number(searchParams?.ordersPage) || 1;
+	const eventsPage = Number(searchParams?.eventsPage) || 1;
+
+	const orders = await getOrdersByUser({ userId, page: ordersPage });
+	const orderEvents = orders?.data.map((order: IOrder) => order.event) || [];
+	const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
+
+	console.log(orderEvents);
+
 	return (
 		<>
 			{/* MY TICKETS */}
@@ -22,18 +33,18 @@ const ProfilePage = async () => {
 				</div>
 			</section>
 
-			{/* <section className="wrapper my-8">
-			<Collection
-					data={events?.data}
+			<section className="wrapper my-8">
+				<Collection
+					data={orderEvents}
 					emptyTitle="No Events Tickets Purchased Yet"
 					emptyStateSubtext="No worries - plenty of exciting events to explore!"
 					collectionType="My_Tickets"
 					limit={3}
-					page={1}
+					page={ordersPage}
 					urlParamName="ordersPage"
-					totalPages={2}
+					totalPages={orders?.totalPages}
 				/>
-			</section> */}
+			</section>
 			{/* EVENT ORGANIZED*/}
 			<section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
 				<div className="wrapper flex items-center justify-center sm:justify-between">
@@ -50,9 +61,9 @@ const ProfilePage = async () => {
 					emptyStateSubtext="Go Create Some Now!"
 					collectionType="Events_Organized"
 					limit={6}
-					page={1}
+					page={eventsPage}
 					urlParamName="eventsPage"
-					totalPages={2}
+					totalPages={organizedEvents?.totalPages}
 				/>
 			</section>
 		</>
