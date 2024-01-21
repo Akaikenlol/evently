0ad2216b2416773b42bca39dd5b25caf753b91c2
Akaikenlol <keithken972@gmail.com>
2024-01-21 20:42:52 +0630

    feat: Search Functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,14 +2,19 @@ import Collection from "@/components/shared/Collection";
 import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+	const page = Number(searchParams?.page) || 1;
+	const searchText = (searchParams?.query as string) || "";
+	const category = (searchParams?.category as string) || "";
+
 	const events = await getAllEvents({
-		query: "",
-		category: "",
-		page: 1,
+		query: searchText,
+		category,
+		page,
 		limit: 6,
 	});
 
